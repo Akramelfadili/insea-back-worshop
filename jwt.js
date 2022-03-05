ffaeba872a3d130dffc2c8491cbf5ad666784435
Akram <fadili261998@gmail.com>
2022-03-05 17:15:15 +0100

    jwt token verification

--- jwt.js
+++ jwt.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import * as config from './config.js'
 
 const { jwt: jwtConfig } = config
@@ -16,4 +17,26 @@ export async function generateJWT({
     }
     const token = jwt.sign(payload, secret)
     return token
+}
+
+
+export function getTokenHeader(req) {
+    const hasToken =
+        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') ||
+        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
+
+    return hasToken ? req.headers.authorization.split(' ')[1] : null
+}
+
+export function getAndVerifyJWT(requestObject) {
+    try {
+        const token = getTokenHeader(requestObject)
+        if (token) {
+            const payload = jwt.verify(token, jwtConfig.secret)
+            return payload
+        }
+        return null
+    } catch (err) {
+        throw new Error('Invalid or Expired token ')
+    }
 }
