fad9492079a4d5a17ea144f012023147c0246586
Akram <fadili261998@gmail.com>
2022-03-05 17:26:38 +0100

    signup controller added

--- database/models/user.js
+++ database/models/user.js
@@ -1,21 +1,35 @@
+import bcrypt from 'bcrypt'
 import pkg from 'mongoose'
-const { Schema } = pkg
+const { Schema, model } = pkg
 
 const UserSchema = new Schema({
     email: {
         type: String,
-        required: true,
     },
     name: {
         type: String,
-        required: true,
+    },
+    avatarUrl: {
+        type: String,
     },
     password: {
         type: String,
-        required: true,
     },
 }, { collection: 'user', timestamps: true }, )
 
-UserSchema.index({ email: 1 }, { unique: true })
-
 export const User = model('User', UserSchema)
+
+const SALT_ROUNDS = 10
+export const hashPassword = (password) => bcrypt.hash(password, SALT_ROUNDS)
+export const comparePassword = async(plainTextPassword, hash) => {
+    return bcrypt.compare(plainTextPassword, hash)
+}
+
+export const createUser = async({ email, name, password }) => {
+    const hashedPassword = await hashPassword(password)
+    const user = await User.create({ email, name, password: hashedPassword })
+    return user
+}
+
+export const getUserById = async(id) => User.findOne({ _id: id }).lean()
+export const getUserByEmail = async(email) => User.findOne({ email: email }).lean()
--- jwt.js
+++ jwt.js
@@ -5,17 +5,19 @@ import * as config from './config.js'
 
 const { jwt: jwtConfig } = config
 
-
 export async function generateJWT({
     expiration = jwtConfig.expiration,
     timeUnit = jwtConfig.timeUnit,
     secret = jwtConfig.secret,
+    user,
 }) {
     const payload = {
         exp: dayjs().add(expiration, timeUnit).unix(),
-        k1: 'v1',
+        user: { id: user._id },
     }
+
     const token = jwt.sign(payload, secret)
+
     return token
 }
 
