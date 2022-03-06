0e7b13aacb7a51f18a3842ea189bccc339dd9b0d
Akram <fadili261998@gmail.com>
2022-03-06 11:06:18 +0100

    auth middl

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,24 +1,27 @@
 import { getUserByEmail, comparePassword } from '../../database/models/user.js'
 import { generateJWT } from '../../jwt.js'
 
+
 async function login(req, res) {
     const { email, password } = req.body
 
-    if (!email || !password) {
+
+    if (!email || !password)
         return res.status(400).send({ message: 'Email and password are required' })
-    }
 
     const userWithEmail = await getUserByEmail(email)
 
-    if (!userWithEmail) {
+    if (!userWithEmail)
         return res.status(401).send({ message: 'Wrong email or password' })
-    }
 
-    const isCorrectPassword = await comparePassword(password, userWithEmail.password)
+    const isCorrectPassword = await comparePassword(
+        password,
+        userWithEmail.password
+    )
 
-    if (!isCorrectPassword) {
+    if (!isCorrectPassword)
         return res.status(401).send({ message: 'Wrong email or password' })
-    }
+
 
     const token = await generateJWT({ user: userWithEmail })
 
